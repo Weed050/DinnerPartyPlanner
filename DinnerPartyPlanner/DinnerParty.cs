d2021c0849f7f8680222f2b048c170c60ead818b
Weed050 <84383140+Weed050@users.noreply.github.com>
2023-03-26 20:25:46 +0200

    it's almost done

--- DinnerPartyPlanner/DinnerParty.cs
+++ DinnerPartyPlanner/DinnerParty.cs
@@ -8,40 +8,42 @@ namespace DinnerPartyPlanner
 {
     public class DinnerParty
     {
+        public DinnerParty(int numberOfPeople, bool healthyOption, bool fancyDecoration)
+        {
+            NumberOfPeople = numberOfPeople;
+            HealthyOption = healthyOption;
+            FancyDecorations = fancyDecoration;
+        }
         public int NumberOfPeople { get; set; }
         public bool FancyDecorations { get; set; }
         public bool HealthyOption { get; set; }
-        public decimal  Cost 
-        { get 
-            { 
-            decimal cost = 0;
-             cost +=   CalculateCostOfBeveragesPerPerson();
-                cost += CalculateCostOfBeveragesPerPerson();
-                cost += NumberOfPeople * 25M;
+        public decimal CostOfFoodPerPerson { get { return 25M; } }
+        public decimal Cost
+        {
+            get
+            {
+                decimal cost = 0;
+                cost = CalculateCostOfDecorations();
+                cost += NumberOfPeople * (CalculateCostOfBeveragesPerPerson() + CostOfFoodPerPerson);
                 if (HealthyOption)
                 {
-                    cost *= 0.95M;
+                    cost = cost * 0.95M;
                 }
                 return cost;
-            }  
+            }
         }
 
-        public DinnerParty(int numberOfPeople,bool healthyOption,bool fancyDecoration)
-        {
-            this.NumberOfPeople = numberOfPeople;
-            this.HealthyOption = healthyOption;
-            this.FancyDecorations = fancyDecoration;
-        }
+
         private decimal CalculateCostOfDecorations()
         {
             decimal cost = 0;
             if (FancyDecorations)
             {
-                cost = (NumberOfPeople * 15M) + 50M;
+                cost = (NumberOfPeople * 15.00M) + 50.00M;
             }
             else
             {
-                cost = (NumberOfPeople * 7.5M) + 30M;
+                cost = (NumberOfPeople * 7.50M) + 30.00M;
             }
             return cost;
         }
@@ -50,11 +52,11 @@ namespace DinnerPartyPlanner
             decimal cost = 0;
             if (HealthyOption)
             {
-                cost = NumberOfPeople * 5M;
+                cost = NumberOfPeople * 5.00M;
             }
             else
             {
-                cost = NumberOfPeople * 20M;
+                cost = NumberOfPeople * 20.00M;
             }
             return cost;
         }
--- DinnerPartyPlanner/Form1.cs
+++ DinnerPartyPlanner/Form1.cs
@@ -12,23 +12,25 @@ namespace DinnerPartyPlanner
         public Form1()
         {
             InitializeComponent();
-            dinnerParty = new DinnerParty((int)numericUpDown1.Value, fancyBox.Checked, healthyBox.Checked);
+            dinnerParty = new DinnerParty((int)numericUpDown1.Value, healthyBox.Checked, fancyBox.Checked);
             DisplayDinnerPartyCost();
         }
 
         private void numericUpDown1_ValueChanged(object sender, EventArgs e)
         {
-            //numericUpDown1.Value
+            dinnerParty.NumberOfPeople = (int)numericUpDown1.Value;
             DisplayDinnerPartyCost();
         }
 
         private void fancyBox_CheckedChanged(object sender, EventArgs e)
         {
+            dinnerParty.FancyDecorations = fancyBox.Checked;
             DisplayDinnerPartyCost();
         }
 
         private void healthyBox_CheckedChanged(object sender, EventArgs e)
         {
+            dinnerParty.HealthyOption = healthyBox.Checked;
             DisplayDinnerPartyCost();
         }
     }
