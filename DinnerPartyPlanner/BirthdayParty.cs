74add66595054181406aab6f222661dc9ff0f905
Weed050 <84383140+Weed050@users.noreply.github.com>
2023-03-27 09:21:29 +0200

    checking lenght of cake

--- DinnerPartyPlanner/BirthdayParty.cs
+++ DinnerPartyPlanner/BirthdayParty.cs
@@ -11,7 +11,30 @@ namespace DinnerPartyPlanner
         public int NumberOfPeople { get; set; }
         public bool FancyDecorations { get; set; }
         public decimal CostOfFoodPerPerson { get { return 25M; } }
-        private string CakeWriting;
+        public string CakeWriting { get; set; }
+        private int ActualLenght { get 
+            {
+                if (CakeWriting.Length > MaxWritingLenght())
+                {
+                    return MaxWritingLenght();
+                }
+                else { return CakeWriting.Length; }
+            }}
+        private int CakeSize()
+        {
+            if (NumberOfPeople <= 4)
+                return 20;
+            
+            else
+                return 40;
+        }
+        private int MaxWritingLenght()
+        {
+            if (CakeSize() == 20)
+                return 16;
+            else
+                return 40;
+        }
         public BirthdayParty(int numberOfPeople,bool fancyDecorations, string cakeWriting)
         {
             NumberOfPeople = numberOfPeople;
